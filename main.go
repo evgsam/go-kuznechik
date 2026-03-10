1b2083ff2f3ae43e724f2a2d734bc7cff413ff0d
evgsam <evgsamarin@gmail.com>
2026-03-10 12:53:36 +0300

    определение прототипов функций

--- main.go
+++ main.go
@@ -8,6 +8,20 @@ type Key256 = [32]byte
 type RoundKey = [16]byte
 type RoundKeys = [10]RoundKey
 
+// Предполагаемые к использованию функции
+// инициалищация контекста шифра
+func NewKuznyechik(key Key256) RoundKeys
+
+// шифрование одного блока
+func EncryptBlock(plaintext Block, rk RoundKeys) (ciphertext Block)
+
+// расшифрование одного блока
+func DecryptBlock(ciphertext Block, rk RoundKeys) (plaintext Block)
+
+// X-преобразования
+func XorBlock(a, b Block) (res Block)
+func XorKey(a, b RoundKey) (res RoundKey)
+
 func main() {
 	fmt.Println("Учебный проект по реализации Кузнечика на go")
 }
