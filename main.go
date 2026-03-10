20642fdbce9177fcf37ef902ebd32aefed020a06
evgsam <evgsamarin@gmail.com>
2026-03-10 12:28:32 +0300

    предполагаемые к использованию типы данных из ГОСТ

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Предполагаемые к использованию в коде типы данных
+type Block = [16]byte
+type Key256 = [32]byte
+type RoundKey = [16]byte
+type RoundKeys = [10]RoundKey
+
 func main() {
 	fmt.Println("Учебный проект по реализации Кузнечика на go")
 }
