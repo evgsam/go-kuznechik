a5979d7df72b1f7ad907a83aea203824f7884dd1
evgsam <evgsamarin@gmail.com>
2026-03-10 15:03:08 +0300

    функция умножения чисел в поле Галуа над неприводимым полиномом x^8 + x^7 + x^6 + x + 1

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ type Key256 = [32]byte
 type RoundKey = [16]byte
 type RoundKeys = [10]RoundKey
 
+// числовое выражение -константа полинома x^8 + x^7 + x^6 + x + 1
 const gf8 = 0xc3
 
 // Предполагаемые к использованию функции
@@ -30,25 +31,31 @@ func XorBlock(a, b Block) (res Block) {
 	return res
 }
 
-func XorKey(a, b RoundKey) (res RoundKey) {
+func XorKey(a, b RoundKey) RoundKey {
 	return XorBlock(a, b)
+}
+
+// функция умножения чисел в поле Галуа над неприводимым полиномом x^8 + x^7 + x^6 + x + 1 (0xc3)
+func GF8Mul(a, b uint8) uint8 {
+	var c uint8
+	c = 0
+	for b != 0 { // проверяем, остались ли биты в b
+		if b&1 != 0 {
+			c = c ^ a
+		}
+		if a&0x80 != 0 {
+			a = (a << 1) ^ gf8
+		} else {
+			a = a << 1
+		}
+		b >>= 1 // переходим к следующему биту
+	}
+	return c
 }
 
 func main() {
 	fmt.Println("Учебный проект по реализации Кузнечика на go")
-	zero := Block{}     // все 0
-	ones := Block{0xFF} // все 0xFF (Go заполнит все байты)
-
-	var result Block
-	var rkResult RoundKey
-
-	result = XorBlock(zero, ones)
-	// result должен быть все 0xFF
-
-	rk1 := RoundKey{0xAA} // все 0xAA
-	rk2 := RoundKey{0x55} // все 0x55
-	rkResult = XorKey(rk1, rk2)
-
-	fmt.Println(result)
-	fmt.Println(rkResult)
+	fmt.Println(GF8Mul(0x01, 0x01)) // Должно быть 0x01
+	fmt.Println(GF8Mul(0x02, 0x01)) // Должно быть 0x02
+	fmt.Println(GF8Mul(0xFF, 0x01)) // Должно быть 0xFF
 }
