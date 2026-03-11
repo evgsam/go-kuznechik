9625faa31040b33b5846766ebbfd58464d417af3
evgsam <evgsamarin@gmail.com>
2026-03-11 22:11:44 +0300

    передача мастерключа через параметр

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"bytes"
+	"flag"
 	"fmt"
+	"os"
 )
 
 // Переменные из стандарта
@@ -23,18 +24,53 @@ func InitTables() {
 	}
 }
 
+func parseMasterKeyFromHex(keyStr string) Key256 {
+	var key Key256
+	if len(keyStr) != 64 {
+		fmt.Fprintf(os.Stderr, "Неверная длина ключа: %d (ожида 64 символа)\n", len(keyStr))
+		os.Exit(1)
+	}
+	for i := 0; i < 32; i++ {
+		var byteVal uint8
+		_, err := fmt.Sscanf(keyStr[i*2:i*2+2], "%2x", &byteVal)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Ошибка парсинга ключа на позиции %d: %v\n", i, err)
+			os.Exit(1)
+		}
+		key[i] = byteVal
+	}
+	return key
+}
+
+func blockEqual(a, b Block) bool {
+	for i := 0; i < 16; i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 // main — точка входа
 func main() {
-	fmt.Println("Учебный проект по реализации Кузнечика на go")
+	// Парсинг аргументов командной строки
+	masterKeyFlag := flag.String("k", "", "Master key в hex формате (32 байта = 64 символа)")
+	flag.Parse()
+
+	var masterkey Key256
+	if *masterKeyFlag != "" {
+		masterkey = parseMasterKeyFromHex(*masterKeyFlag)
+	} else {
+		fmt.Println("Внимание: Master key не указан, используется дефолтный")
+		masterkey = Key256{
+			0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
+			0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+			0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10,
+			0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+		}
+	}
 
 	InitTables()
-
-	masterkey := Key256{
-		0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
-		0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-		0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10,
-		0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
-	}
 
 	plaintext := RoundKey{
 		0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x00,
@@ -54,13 +90,13 @@ func main() {
 	fmt.Printf("Шифртекст:       % x\n", ciphertext)
 	fmt.Printf("Расшифровка:     % x\n", plaintext2)
 
-	if bytes.Equal(ciphertext[:], expected[:]) {
+	if blockEqual(ciphertext, expected) {
 		fmt.Println("Шифрование работает")
 	} else {
 		fmt.Println("Ошибка в шифровании")
 	}
 
-	if bytes.Equal(plaintext2[:], plaintext[:]) {
+	if blockEqual(plaintext2, plaintext) {
 		fmt.Println("Расшифрование работает")
 	} else {
 		fmt.Println("Ошибка в расшифровке")
